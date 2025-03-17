a9a739e199413f2a981fbff9e095269762fafc93
coderahulmishra <coderahulmishra@gmail.com>
2025-03-17 12:30:59 +0530

    Day 2

--- Day 1/app.js
+++ Day 1/app.js
@@ -7,7 +7,11 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const app = express();
+
+const SECRET_KEY = "mySecretKey"
 
 const User = require('./models/user');
 
@@ -79,4 +83,51 @@ app.get("/contact",(req,res) => {
 
 app.listen(3000,() => {
     console.log(`server running on: http://localhost:3000`);
+})
+
+//Signup Route
+app.post("/signup",async (req,res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).send("Email already registered");
+        }
+        //hash password
+        const hashedPassword = await bcrypt.hash(password,10);
+        //save user
+        const newUser = new User({name,email,password:hashedPassword});
+        await newUser.save();
+
+        res.send("User registered successfully!");
+    } catch (error) {
+        res.status(500).send("Error signing up");
+    }
+})
+
+//login route
+app.post("/login", async(req,res) => {
+    try {
+        const {email,password} = req.body;
+
+        //check if user exists
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).send("User not found");
+        }
+
+        //Compare password
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).send("Invalid password");
+        }
+
+        //Generate JWT Token
+        const token = jwt.sign({userId: user._id},SECRET_KEY,{expiresIn:"1h"});
+
+        res.json({message: "Login successful",token});
+    } catch (error) {
+        res.status(500).send("Error logging in");
+    }
 })
